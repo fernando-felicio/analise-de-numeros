4fe2359eb7a5584467c5eb3bcc902e4dd02c94d3
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-24 16:58:59 -0300

    Primeiro commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+package analiseNumerica;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o número que será analisado:");
+		int X = sc.nextInt();
+		int qtdImpar = 0;
+		
+		for (int numeroImpar = 0; numeroImpar < X; numeroImpar ++) {
+			if (numeroImpar %2 != 0) {
+				qtdImpar += 1;
+				System.out.println(numeroImpar);				
+			}			
+		}
+		
+		
+	}
+
+}
