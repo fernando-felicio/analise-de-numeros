405c3739d74b5e819c5bd1e88fdbea05c01dd9c0
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-24 17:00:45 -0300

    Adicionando contador de números ímpares

--- Main.java
+++ Main.java
@@ -16,7 +16,8 @@ public class Main {
 				System.out.println(numeroImpar);				
 			}			
 		}
-		
+		System.out.printf("O número analisado foi o %d%nA quantidade de números impar é de: %d",X, qtdImpar);
+
 		
 	}
 
